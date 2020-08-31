0d03b137641de7c2b6131c1dde54769f63924323
Nelson <nelson119@outlook.com>
2020-09-01 01:06:53 +0800

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,30 @@
+'use strict';
+/*eslint-disable new-cap, no-unused-vars,
+	no-use-before-define, no-trailing-spaces, space-infix-ops, comma-spacing,
+	no-mixed-spaces-and-tabs, no-multi-spaces, camelcase, no-loop-func,no-empty,
+	key-spacing ,curly, no-shadow, no-return-assign, no-redeclare, no-unused-vars,
+	eqeqeq, no-extend-native, quotes , no-inner-declarations*/
+/*global  $, jQuery, TweenMax */
+var app = {};
+app.partial = {};
+
+if($ || jQuery){
+	app.$ = $.noConflict() || jQuery.noConflict();
+}
+(function($){
+	$(function(){
+		
+		// 定義每個元件
+		$.each(app.partial, function(name, init){
+			var cont = $('[role='+name+']');
+			init($, cont);
+		});
+
+
+
+	});
+}(app.$));
+
+
+
+
